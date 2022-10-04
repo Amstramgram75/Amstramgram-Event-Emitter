abcbe4bac61e3113b906daeb044afa17e16819af
Amstramgram75 <laurentneumann@free.fr>
2022-10-04 15:49:08 +0200

    Initial

--- src/amstramgramEventEmitter.js
+++ src/amstramgramEventEmitter.js
@@ -6,8 +6,10 @@ export default class AmstramgramEventEmitter {
   #eventsOnce = {}
   #eventAdded
   #eventRemoved
+  #onEventAddedCallback = false
+  #onEventRemovedCallback = false
 
-  
+
 
 
   /***********************************
@@ -15,30 +17,25 @@ export default class AmstramgramEventEmitter {
    ***********************************/
   /**
    * Utility function to register events/callbacks set with the on() and once() methods
-   * @param {*} eventsNames 
-   * @param {*} callback 
-   * @param {*} once 
+   * @param {String} eventsNames 
+   * @param {Function} callback 
+   * @param {Boolean} [once = false]
    */
   #registerEvents(eventsNames, callback, once = false) {
     eventsNames.split(' ').forEach(eventName => {
       let added = false
       const
-        //Collect the registered callbacks for the given eventName. If none, define a new Set
-        events = this.#events[eventName] || new Set(),
-        //Collect the registered once callbacks for the given eventName. If none, define a new Set
-        eventsOnce = this.#eventsOnce[eventName] || new Set(),
-        //union is a new set of the all the registered callbacks
-        union = new Set([...eventsOnce, ...events]),
+        //union is a new set of all the registered callbacks
+        union = new Set([...(this.#events[eventName] || new Set()), ...(this.#eventsOnce[eventName] || new Set())]),
         //Get a reference to the events object in accordance with the once parameter
-        eventsCollection = once ? this.#eventsOnce : this.#events,
-        //Get a reference to the set. If it not yet exists, instantiate a new one
-        set =  eventsCollection[eventName] || (eventsCollection[eventName] = new Set())
+        eventsCollection = once ? this.#eventsOnce : this.#events
       if (!union.has(callback)) {//If the callback is not yet registered
         added = true
+        const set = eventsCollection[eventName] || (eventsCollection[eventName] = new Set())
         set.add(callback)
       }
       //Emit the eventadded event
-      if (this.#eventAdded) this.emit(this.#eventAdded, {event: eventName, callback: callback, success: added, list: this.eventsAndCallbacksList})
+      if (this.#eventAdded) this.emit(this.#eventAdded, { event: eventName, callback: callback, success: added, list: this.eventsAndCallbacksList })
     })
   }
 
@@ -48,11 +45,13 @@ export default class AmstramgramEventEmitter {
   #unregisterAll() {
     [this.#events, this.#eventsOnce].forEach(eventCollection => {
       Object.keys(eventCollection).forEach(eventName => {
-        eventCollection[eventName].forEach(callback => {
-          eventCollection[eventName].delete(callback)
-          if (this.#eventRemoved) this.emit(this.#eventRemoved, {event: eventName, callback: callback, success: true, list: this.eventsAndCallbacksList})
-        })
-        delete eventCollection[eventName]
+        if (eventName != this.#eventAdded && eventName != this.#eventRemoved) {
+          eventCollection[eventName].forEach(callback => {
+            eventCollection[eventName].delete(callback)
+            if (this.#eventRemoved) this.emit(this.#eventRemoved, { event: eventName, callback: callback, success: true, list: this.eventsAndCallbacksList })
+          })
+          delete eventCollection[eventName]
+        }
       })
     })
   }
@@ -62,18 +61,19 @@ export default class AmstramgramEventEmitter {
    * @param {String} eventName : event to be cleaned
    */
   #unregisterEvent(eventName) {
+    if (eventName == this.#eventAdded || eventName == this.#eventRemoved) return;
     let removed = false
     [this.#events, this.#eventsOnce].forEach(eventsCollection => {
       if (typeof eventsCollection[eventName] !== 'undefined') {
         eventsCollection[eventName].forEach(callback => {
           eventsCollection[eventName].delete(callback)
           removed = true
-          if (this.#eventRemoved) this.emit(this.#eventRemoved, {event: eventName, callback: callback, success: true, list: this.eventsAndCallbacksList})
+          if (this.#eventRemoved) this.emit(this.#eventRemoved, { event: eventName, callback: callback, success: true, list: this.eventsAndCallbacksList })
         })
         delete eventsCollection[eventName]
       }
     })
-    if (!removed && this.#eventRemoved) this.emit(this.#eventRemoved, {event: eventName, callback: '', success: false, list: this.eventsAndCallbacksList})
+    if (!removed && this.#eventRemoved) this.emit(this.#eventRemoved, { event: eventName, callback: '', success: false, list: this.eventsAndCallbacksList })
   }
 
   /**
@@ -84,14 +84,14 @@ export default class AmstramgramEventEmitter {
   #unregisterCallback(eventName, callback) {
     let removed = false;
     [this.#events, this.#eventsOnce].forEach(eventsCollection => {
-     if (typeof eventsCollection[eventName] !== 'undefined') {
+      if (typeof eventsCollection[eventName] !== 'undefined') {
         eventsCollection[eventName].delete(callback)
         if (eventsCollection[eventName].size == 0) delete eventsCollection[eventName]
         removed = true
-        if (this.#eventRemoved) this.emit(this.#eventRemoved, {event: eventName, callback: callback, success: removed, list: this.eventsAndCallbacksList})
+        if (this.#eventRemoved) this.emit(this.#eventRemoved, { event: eventName, callback: callback, success: removed, list: this.eventsAndCallbacksList })
       }
     })
-    if (!removed && this.#eventRemoved) this.emit(this.#eventRemoved, {event: eventName, callback: callback, success: removed, list: this.eventsAndCallbacksList})
+    if (!removed && this.#eventRemoved) this.emit(this.#eventRemoved, { event: eventName, callback: callback, success: removed, list: this.eventsAndCallbacksList })
   }
 
 
@@ -101,15 +101,18 @@ export default class AmstramgramEventEmitter {
    *           CONSTRUCTOR           *
    ***********************************/
   /**
-   * @param {String} eventAdded : name of the event emitted when a new event/callback is added
+   * @param {String} [eventAdded = "eventadded"] name of the event emitted when a new event/callback is added.
    * If it's an empty string or not a string, no event will be dispatched
-   * @param {String} eventRemoved: name of the event emitted when a new event/callback is removed 
+   * @param {String} [eventRemoved = "eventremoved"] name of the event emitted when a new event/callback is removed.
    * If it's an empty string or not a string, no event will be dispatched
    */
-  constructor(eventAdded = 'eventadded', eventRemoved = 'eventremoved') {
+  constructor(onEventAddedCallback = false, onEventRemovedCallback = false) {
+  // constructor(eventAdded = 'eventadded', eventRemoved = 'eventremoved') {
     //check if the parameters are string without spaces
-    this.#eventAdded = typeof eventAdded === 'string' && !/\s/g.test(eventAdded) ? eventAdded : ''
-    this.#eventRemoved = typeof eventRemoved === 'string' && !/\s/g.test(eventRemoved) ? eventRemoved : ''
+    this.#onEventAddedCallback = typeof onEventAddedCallback === 'function' ? onEventAddedCallback : null
+    this.#onEventRemovedCallback = typeof onEventRemovedCallback === 'function' ? onEventRemovedCallback : null
+    // this.#eventAdded = typeof eventAdded === 'string' && !/\s/g.test(eventAdded) ? eventAdded : ''
+    // this.#eventRemoved = typeof eventRemoved === 'string' && !/\s/g.test(eventRemoved) ? eventRemoved : ''
   }
 
 
@@ -133,7 +136,8 @@ export default class AmstramgramEventEmitter {
    * }
    */
   get eventsAndCallbacksList() {
-    let list = {},
+    let 
+      list = {},
       eventNamesArrays = [Object.keys(this.#events), Object.keys(this.#eventsOnce).filter(eventName => !Object.keys(this.#events).includes(eventName))]
     eventNamesArrays.forEach(array => array.forEach(eventName => {
       let o = {}
@@ -146,7 +150,7 @@ export default class AmstramgramEventEmitter {
 
 
 
-  
+
   /***********************************
    *              METHODS            *
    ***********************************/
@@ -161,8 +165,8 @@ export default class AmstramgramEventEmitter {
   }
 
   /**
-   * @param {String} eventsNames : list of events names separated by a space 
-   * @param {*} callback : callback to be registered once for the given events
+   * @param {String} eventsNames - list of events names separated by a space 
+   * @param {Function} callback - callback to be registered once for the given events
    * @returns this so methods can be chained
    */
   once(eventsNames, callback) {
@@ -171,9 +175,9 @@ export default class AmstramgramEventEmitter {
   }
 
   /**
-   * @param {String} eventsNames @default {''} : list of events names separated by a space
+   * @param {String} [eventsNames = ''] : list of events names separated by a space
    * If empty string, all the callbacks for all the events are unregistered
-   * @param {function} callback @default {undefined} : callback to be unregistered for the given events
+   * @param {Function} [callback = undefined] - callback to be unregistered for the given events
    * If undefined, all the callbacks for all the given events are unregistered
    * @returns this so methods can be chained
    */
